122a10fbf7b371cbb6983e5424178de2661562c5
Gaspard Quentin <gaspard.quentin1905@gmail.com>
2024-11-30 17:16:37 +0100

    feat: factories and menu

--- reglex/scripts/factory.gd
+++ reglex/scripts/factory.gd
@@ -1,19 +1,36 @@
 extends Area2D
 
+const FactoryType = preload("res://scripts/factory_type.gd")
+
+var type: FactoryType.Type;
+var is_set_up: bool = false
+var amount_per_harvest: int = 1
+
+func set_up(t: FactoryType.Type) -> void:
+	is_set_up = true
+	type = t
+	
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Clock.timeout.connect(place_product)
+	#set_up(FactoryType.Type.SERVIETTE)
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.pressed:
 		get_product()
 		$Clock.start()
 		print("get serviette")
+		harvest()
 
 func place_product() -> void:
 	$ProductCollider.disabled = false
 	$ProductCollider/Product.visible = true
+	
 
 func get_product() -> void:
 	$ProductCollider.disabled = true
 	$ProductCollider/Product.visible = false
+
+func harvest() -> void:
+	get_parent().get_node("GameManager").add_produits_menstruels(type, amount_per_harvest)
--- reglex/scripts/game_manager.gd
+++ reglex/scripts/game_manager.gd
@@ -1,11 +1,17 @@
 extends Node
 
+const FactoryType = preload("res://scripts/factory_type.gd")
+
 var LABS = preload("res://scenes/lab.tscn");
+var FACTORIES = preload("res://scenes/factory.tscn");
 var ShopMenu = null;
 
 #instantiate all metrics
 var research_points: int = 0
 var satisfaction: int = 0
+var stocks_menstruel = {
+	FactoryType.Type.SERVIETTE: 0,
+}
 
 var selected_building: String = "";
 
@@ -14,10 +20,13 @@ func add_research_points(points: int) -> void:
 	#$UI.update_research_points(research_points)
 	print(research_points)
 
+func add_produits_menstruels(type, amount):
+	stocks_menstruel[type] += amount
+	print(stocks_menstruel)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	ShopMenu = get_parent().get_node("ShopMenu/PopupMenu")
+	ShopMenu = get_parent().get_node("ShopMenu")
 	ShopMenu.visible = false
 
 
@@ -36,6 +45,9 @@ func place_building(click_position: Vector2) -> void:
 	match selected_building:
 		"Laboratoire":
 			node = LABS.instantiate()
+		"Serviette":
+			node = FACTORIES.instantiate()
+			node.set_up(FactoryType.Type.SERVIETTE)
 		_:
 			return
 	add_child(node)
@@ -49,5 +61,4 @@ func set_selected_building(building: String):
 
 func _on_shop_button_pressed() -> void:
 	ShopMenu.visible = !ShopMenu.visible
-	
 	
